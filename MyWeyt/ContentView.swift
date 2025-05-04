36b59822587bbf71342dbb7039de758369d5ad6e
Sefa Sarikaya <sarikayasefa666@gmail.com>
2025-05-04 04:30:05 +0200

    Initial Commit

---
+++ MyWeyt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWeyt
+//
+//  Created by Sefa Sarikaya on 04.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWeyt/MyWeytApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeytApp.swift
+//  MyWeyt
+//
+//  Created by Sefa Sarikaya on 04.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MyWeytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
